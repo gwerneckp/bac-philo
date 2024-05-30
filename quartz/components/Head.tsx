b824fe2fa37fa8e8960cc335aabfb6ac45212074
gwerneckpaiva <gwerneckpaiva@gmail.com>
2024-05-30 12:58:14 +0100

    chore: Add shortcut icon to Head component

--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -39,6 +39,7 @@ export default (() => {
         <link rel="apple-touch-icon" sizes="180x180" href="/static/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/static/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/static/favicon-16x16.png" />
+        <link rel="shortcut icon" href="/icon/favicon.ico" />
         <link rel="manifest" href="/static/site.webmanifest" />
         <meta name="description" content={description} />
         <meta name="generator" content="Quartz" />
