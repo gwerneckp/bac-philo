fd2a498c69cbe209fc77b94f8153ccc664bb29b4
gwerneckpaiva <gwerneckpaiva@gmail.com>
2024-05-30 12:53:44 +0100

    chore: Update static file paths in Head component

--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -36,10 +36,10 @@ export default (() => {
         <meta property="og:width" content="1200" />
         <meta property="og:height" content="675" />
         {/* <link rel="icon" href={iconPath} /> */}
-        <link rel="apple-touch-icon" sizes="180x180" href="static/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="static/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="static/favicon-16x16.png" />
-        <link rel="manifest" href="static/site.webmanifest" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/static/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/static/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/static/favicon-16x16.png" />
+        <link rel="manifest" href="/static/site.webmanifest" />
         <meta name="description" content={description} />
         <meta name="generator" content="Quartz" />
         {css.map((href) => (
